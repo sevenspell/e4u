38bb0bc22c829d50985e332ba67e95d4377b81b7
Seven <sevenspell@gmail.com>
2020-07-21 02:24:09 +1000

    Updates

---
+++ precache-manifest.6f8c458c54bbaccfe0270b59db99ed40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8fd9d190c77324a2e710ef35e7ce41",
+    "url": "/e4u/index.html"
+  },
+  {
+    "revision": "56e138177717bb52609b",
+    "url": "/e4u/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3456815eb958d7a9eb4c",
+    "url": "/e4u/static/js/2.79166dc5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/e4u/static/js/2.79166dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e138177717bb52609b",
+    "url": "/e4u/static/js/main.df58a443.chunk.js"
+  },
+  {
+    "revision": "5a17dad84e4ba5e53fdf",
+    "url": "/e4u/static/js/runtime-main.eea59773.js"
+  },
+  {
+    "revision": "68ba501cab184fee8ae331b74708eabf",
+    "url": "/e4u/static/media/egimage.68ba501c.png"
+  }
+]);
